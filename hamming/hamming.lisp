e175ae587f79f678d545ba79c49f0caa843d31b8
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-17 23:25:55 +0200

    Solve exercise "Hamming"

--- hamming/hamming.lisp
+++ hamming/hamming.lisp
@@ -1,9 +1,16 @@
 (defpackage #:hamming
   (:use #:cl)
   (:export #:distance))
-
 (in-package #:hamming)
 
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
+  (if (not (eq (length dna1) (length dna2))) nil
+      (length
+       (delete-if #'(lambda (n) (equalp (first n) (second n)))
+                  (mapcar #'list (coerce dna1 'list) (coerce dna2 'list))
+                  )
+       )
+      )
   )
+
