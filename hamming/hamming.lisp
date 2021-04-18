683adfea9565cbea92ae1faccb84ea5519a66536
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-18 17:27:00 +0200

    Solve exercise "Hamming" (cont.)
    Changed (again) the equality predicate used in the erroring test.

--- hamming/hamming.lisp
+++ hamming/hamming.lisp
@@ -5,7 +5,7 @@
 
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
-  (when (eql (length dna1) (length dna2))
+  (when (= (length dna1) (length dna2))
       (length
        (delete-if #'(lambda (n) (equalp (first n) (second n)))
                   (mapcar #'list (coerce dna1 'list) (coerce dna2 'list))
