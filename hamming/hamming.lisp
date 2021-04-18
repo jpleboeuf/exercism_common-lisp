4fea5b97b568ce64072b76cf9fca3bee7ea747a7
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-18 17:20:55 +0200

    Solve exercise "Hamming" (cont.)
    Simplified the erroring check.

--- hamming/hamming.lisp
+++ hamming/hamming.lisp
@@ -5,7 +5,7 @@
 
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
-  (if (not (eql (length dna1) (length dna2))) nil
+  (when (eql (length dna1) (length dna2))
       (length
        (delete-if #'(lambda (n) (equalp (first n) (second n)))
                   (mapcar #'list (coerce dna1 'list) (coerce dna2 'list))
