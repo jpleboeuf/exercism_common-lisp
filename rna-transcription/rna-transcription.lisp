5996218dea34caef49b9491bc1dbeae0e89854e1
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-21 22:06:14 +0200

    Solve exercise "RNA Transcription"

--- rna-transcription/rna-transcription.lisp
+++ rna-transcription/rna-transcription.lisp
@@ -4,6 +4,17 @@
   (:export #:to-rna))
 (in-package #:rna-transcription)
 
+(defvar nucleotideTransTbl (make-hash-table))
+(setf (gethash #\G nucleotideTransTbl) #\C)
+(setf (gethash #\C nucleotideTransTbl) #\G)
+(setf (gethash #\T nucleotideTransTbl) #\A)
+(setf (gethash #\A nucleotideTransTbl) #\U)
+
 (defun to-rna (str)
   "Transcribe a string representing DNA nucleotides to RNA."
+  (coerce
+   (mapcar (lambda (n) (gethash n nucleotideTransTbl)) (coerce str 'list))
+   'string
+   )
   )
+
