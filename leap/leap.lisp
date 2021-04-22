37f5da880a1b2cea971841756bcc4945685c4e84
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-22 22:35:34 +0200

    Solve exercise "Leap"

--- leap/leap.lisp
+++ leap/leap.lisp
@@ -3,5 +3,23 @@
   (:export #:leap-year-p))
 (in-package #:leap)
 
-(defun leap-year-p (year)
+(declaim (inline divp))
+(defun divp (a b)
+  (zerop (mod a b))
   )
+
+(declaim (inline !divp))
+(defun !divp (a b)
+  (not (divp a b))
+  )
+
+(defun leap-year-p (year)
+  (and
+   (divp year 4)
+   (or
+    (!divp year 100)
+    (divp year 400)
+    )
+   )
+  )
+
